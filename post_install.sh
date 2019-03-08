36f522d321bdbdeb2ae0b811c2347ffdefbbd42e
Jon <coverthis@gmail.com>
2019-03-08 11:47:21 -0500

    move to nginx

--- post_install.sh
+++ post_install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # Enable the services
-sysrc -f /etc/rc.conf apache24_enable="YES"
+sysrc -f /etc/rc.conf nginx_enable="YES"
 sysrc -f /etc/rc.conf mysql_enable="YES"
+sysrc -f /etc/rc.conf clonedeploy_enable="YES"
 
 # Start the services
 service mysql-server start 
@@ -15,13 +16,13 @@ rand_key=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 wget "https://sourceforge.net/projects/clonedeploy/files/CloneDeploy 1.4.0/clonedeploy-1.4.0.tar.gz"
 tar xvzf clonedeploy-1.4.0.tar.gz
 cd clonedeploy
-mkdir /usr/local/www/apache24/data/clonedeploy
-cp -r frontend /usr/local/www/apache24/data/clonedeploy
-cp -r api /usr/local/www/apache24/data/clonedeploy
+mkdir /usr/local/www/nginx/clonedeploy
+cp -r frontend /usr/local/www/nginx/clonedeploy
+cp -r api /usr/local/www/nginx/clonedeploy
 mkdir /.mono
-sed -i "" "s/xx_marker1_xx/$sql_pass/" /usr/local/www/apache24/data/clonedeploy/api/Web.config
-sed -i "" "s/xx_marker2_xx/$rand_key/" /usr/local/www/apache24/data/clonedeploy/api/Web.config
-echo "Local,http://localhost" > /usr/local/www/apache24/data/clonedeploy/frontend/serverlist.csv
+sed -i "" "s/xx_marker1_xx/$sql_pass/" /usr/local/www/nginx/clonedeploy/api/Web.config
+sed -i "" "s/xx_marker2_xx/$rand_key/" /usr/local/www/nginx/clonedeploy/api/Web.config
+echo "Local,http://localhost/" > /usr/local/www/nginx/clonedeploy/frontend/serverlist.csv
 
 # Setup Tftp
 cp -r tftpboot /
@@ -39,7 +40,7 @@ mysql -u root --password=${sql_pass} clonedeploy < /clonedeploy/cd.sql
 mysql -u root --password=${sql_pass} -e  "GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' identified by '${sql_pass}';flush privileges;"
 
 # Set permissions
-chown -R www:www /tftpboot /cd_dp /usr/local/www/apache24/data/clonedeploy /.mono
+chown -R www:www /tftpboot /cd_dp /usr/local/www/nginx/clonedeploy /.mono
 chmod 1777 /tmp
 
 # make udpcast
@@ -49,4 +50,5 @@ make install
 
 # Restart services
 service mysql-server restart 2>/dev/null
-service apache24 restart 2>/dev/null
+service nginx start 2>/dev/null
+service clonedeploy start 2>/dev/null
